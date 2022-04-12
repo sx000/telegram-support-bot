a4a46c6c8d5e2736438b32028581e06609fdbc38
Pavel <sx000@1-tn.com>
2022-04-12 20:47:10 +0200

    format get text in chat

--- handlers.py
+++ handlers.py
@@ -9,10 +9,13 @@ def start(update, context):
 
     user_info = update.message.from_user.to_dict()
 
+
     context.bot.send_message(
         chat_id=TELEGRAM_SUPPORT_CHAT_ID,
         text=f"""
-📞 Connected {user_info}.
+        username: {user_info["username"]}
+        ID телеграмм: {user_info["id"]}
+        ID заказа: {update.message.text}
         """,
     )
 
@@ -21,9 +24,9 @@ def forward_to_chat(update, context):
     """{ 
         'message_id': 5, 
         'date': 1605106546, 
-        'chat': {'id': 49820636, 'type': 'private', 'username': 'danokhlopkov', 'first_name': 'Daniil', 'last_name': 'Okhlopkov'}, 
+        'chat': {'id': 49820636, 'type': 'private', 'username': 'danokhlopkov', 'https://github.com/sx000/telegram-support-bot': 'Daniil', 'last_name': 'Okhlopkov'}, 
         'text': 'TEST QOO', 'entities': [], 'caption_entities': [], 'photo': [], 'new_chat_members': [], 'new_chat_photo': [], 'delete_chat_photo': False, 'group_chat_created': False, 'supergroup_chat_created': False, 'channel_chat_created': False, 
-        'from': {'id': 49820636, 'first_name': 'Daniil', 'is_bot': False, 'last_name': 'Okhlopkov', 'username': 'danokhlopkov', 'language_code': 'en'}
+        'from': {'id': 49820636, 'https://github.com/sx000/telegram-support-bot': 'Daniil', 'is_bot': False, 'last_name': 'Okhlopkov', 'username': 'danokhlopkov', 'language_code': 'en'}
     }"""
     forwarded = update.message.forward(chat_id=TELEGRAM_SUPPORT_CHAT_ID)
     if not forwarded.forward_from:
@@ -41,15 +44,15 @@ def forward_to_user(update, context):
         'reply_to_message': {
             'message_id': 9, 'date': 1605106659, 
             'chat': {'id': -484179205, 'type': 'group', 'title': '☎️ SUPPORT CHAT', 'all_members_are_administrators': True}, 
-            'forward_from': {'id': 49820636, 'first_name': 'Daniil', 'is_bot': False, 'last_name': 'Okhlopkov', 'danokhlopkov': 'okhlopkov', 'language_code': 'en'}, 
+            'forward_from': {'id': 49820636, 'https://github.com/sx000/telegram-support-bot': 'Daniil', 'is_bot': False, 'last_name': 'Okhlopkov', 'danokhlopkov': 'okhlopkov', 'language_code': 'en'}, 
             'forward_date': 1605106658, 
             'text': 'g', 'entities': [], 'caption_entities': [], 'photo': [], 'new_chat_members': [], 'new_chat_photo': [], 
             'delete_chat_photo': False, 'group_chat_created': False, 'supergroup_chat_created': False, 'channel_chat_created': False, 
-            'from': {'id': 1440913096, 'first_name': 'SUPPORT', 'is_bot': True, 'username': 'lolkek'}
+            'from': {'id': 1440913096, 'https://github.com/sx000/telegram-support-bot': 'SUPPORT', 'is_bot': True, 'username': 'lolkek'}
         }, 
         'text': 'ggg', 'entities': [], 'caption_entities': [], 'photo': [], 'new_chat_members': [], 'new_chat_photo': [], 'delete_chat_photo': False, 
         'group_chat_created': False, 'supergroup_chat_created': False, 'channel_chat_created': False, 
-        'from': {'id': 49820636, 'first_name': 'Daniil', 'is_bot': False, 'last_name': 'Okhlopkov', 'username': 'danokhlopkov', 'language_code': 'en'}
+        'from': {'id': 49820636, 'https://github.com/sx000/telegram-support-bot': 'Daniil', 'is_bot': False, 'last_name': 'Okhlopkov', 'username': 'danokhlopkov', 'language_code': 'en'}
     }"""
     user_id = None
     if update.message.reply_to_message.forward_from:
